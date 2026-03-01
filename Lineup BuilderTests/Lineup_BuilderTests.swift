3c0a9f7f5ad5fdca97964087f93d17d31e030a7d
Nick Davies <nick_davies@outlook.com>
2026-02-28 16:49:16 -0800

    Initial Commit

---
+++ Lineup BuilderTests/Lineup_BuilderTests.swift
@@ -0,0 +1,16 @@
+//
+//  Lineup_BuilderTests.swift
+//  Lineup BuilderTests
+//
+//  Created by Nick Davies on 2/28/26.
+//
+
+import Testing
+
+struct Lineup_BuilderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
