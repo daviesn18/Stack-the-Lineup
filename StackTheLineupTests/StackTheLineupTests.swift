4dce0ed65a3e61e222bfb2a08f4c12c2a41d14c3
Nick Davies <nick_davies@outlook.com>
2026-04-28 10:27:07 -0700

    v2.2 build 23 - fix iCloud data loss, safe Lineup decode, scrollable WhatsNew Added Automated Tests

---
+++ StackTheLineupTests/StackTheLineupTests.swift
@@ -0,0 +1,18 @@
+//
+//  StackTheLineupTests.swift
+//  StackTheLineupTests
+//
+//  Created by Nick Davies on 4/28/26.
+//
+
+import Testing
+
+struct StackTheLineupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
